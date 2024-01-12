399d32f09285e9040832563e39bac6e7b2af62e6
Gautier <bonhur.gautier@gmail.com>
2024-01-12 16:51:03 +0100

    [ADD] create a realease in a .zip format

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Créez un nom de dossier .zip avec la date d'aujourd'hui
+current_date=$(date +"%Y-%m-%d")
+zip_folder_name="r_type_$current_date"
+
 # Create build directory
 mkdir -p server/build
 cd server/build && cmake .. && make && mv r-type_server ../../
 
 cd ../../client && make && mv r-type_client ../
+
+# Créez l'archive .zip en spécifiant le chemin complet des fichiers
+cd .. && zip -r "$zip_folder_name.zip" r-type_client r-type_server assets
